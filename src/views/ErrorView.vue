706a476d5e6eb58d4675f18ca9da772d819711a8
KielRodriguez <kr.rdz.20@gmail.com>
2022-07-11 00:39:56 -0500

    implment not found error

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed">
+    <div class="flex justify-center items-center h-full">
+        <div class="text-center px-6 md:px-12">
+          <h1 class="text-5xl font-bold mt-0 mb-6">404 Error</h1>
+          <h3 class="text-3xl font-bold mt-0 mb-6">Sorry, this page not exist :'(</h3>
+          <router-link to="/" class="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4">
+            Back to the home page
+          </router-link>
+
+        </div>
+    </div>
+  </div>
+</template>
